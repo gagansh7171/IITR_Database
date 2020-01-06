930e40f61cd1357beed73470bc837c4c74e0b8aa
Gagan Sharma <52528493+gagansh7171@users.noreply.github.com>
2020-01-06 12:26:09 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "localhost";
-$user = "test" ;
-$pass = "helloiitr";
+$user = "********" ;
+$pass = "********";
 $db = "accounts";
 $mysqli = new mysqli($host, $user, $pass,$db) or die($mysqli->error);
 
